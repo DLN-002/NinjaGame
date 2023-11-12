5b01943afd4346329e2a0022b7a37b97adf560a5
DLN-002 <146394752+DLN-002@users.noreply.github.com>
2023-11-12 11:29:43 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,44 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "ninjaD":return img`
-. . . . f f f f . . . . . 
-. . f f f f f f f f . . . 
-. f f f f f f c f f f . . 
-f f f f f f c c f f f c . 
-f f f c f f f f f f f c . 
-c c c f f f e e f f c c . 
-f f f f f e e f f c c f . 
-f f f b f e e f b f f f . 
-. f 4 1 f 4 4 f 1 4 f . . 
-. f e 4 4 4 4 4 4 e f . . 
-. f f f e e e e f f f . . 
-f e f f f f f f f f e f . 
-e 4 1 1 1 1 1 1 1 1 4 e . 
-e e f f f 1 f f f f e e . 
-. . . f f f f f f . . . . 
-. . . f f . . f f . . . . 
-`;
-            case "image4":
-            case "ninjaU":return img`
-. . . . f f f f . . . . . 
-. . f f c c c c f f . . . 
-. f f c c c c c c f f . . 
-f f c c c c c c c c f f . 
-f f c c f c c c c c c f . 
-f f f f f c c c f c c f . 
-f f f f c c c f c c f f . 
-f f f f f f f f f f f f . 
-f f f f f f f f f f f f . 
-. f f f f f f f f f f . . 
-. f f f f f f f f f f . . 
-f e f f f f f f f f e f . 
-e 4 1 1 1 1 1 1 1 1 4 e . 
-e e f f f f f f f f e e . 
-. . . f f f f f f . . . . 
-. . . f f . . f f . . . . 
-`;
             case "image3":
             case "ninjaL":return img`
 . . . . . f f f f f . . . 
@@ -97,6 +59,44 @@ f f e e f b f e e f f . .
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "ninjaD":return img`
+. . . . f f f f . . . . 
+. . f f f f f f f f . . 
+. f f f f f f c f f f . 
+f f f f f f c c f f f c 
+f f f c f f f f f f f c 
+c c c f f f e e f f c c 
+f f f f f e e f f c c f 
+f f f b f e e f b f f f 
+. f 4 1 f 4 4 f 1 4 f . 
+. f e 4 4 4 4 4 4 e f . 
+. f f f e e e e f f f . 
+f e f f f f f f f f e f 
+e 4 1 1 1 1 1 1 1 1 4 e 
+e e f f f 1 f f f f e e 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
+            case "image4":
+            case "ninjaU":return img`
+. . . . f f f f . . . . 
+. . f f c c c c f f . . 
+. f f c c c c c c f f . 
+f f c c c c c c c c f f 
+f f c c f c c c c c c f 
+f f f f f c c c f c c f 
+f f f f c c c f c c f f 
+f f f f f f f f f f f f 
+f f f f f f f f f f f f 
+. f f f f f f f f f f . 
+. f f f f f f f f f f . 
+f e f f f f f f f f e f 
+e 4 1 1 1 1 1 1 1 1 4 e 
+e e f f f f f f f f e e 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace SpriteKind {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
     info.changeScoreBy(1)
-    tiles.setTileAt(location, sprites.castle.tilePath5)
+    tiles.setTileAt(location, assets.tile`myTile1`)
 })
 scene.onOverlapTile(SpriteKind.shuriken, assets.tile`myTile3`, function (sprite, location) {
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
@@ -13,22 +13,49 @@ scene.onOverlapTile(SpriteKind.shuriken, assets.tile`myTile3`, function (sprite,
     sprites.destroy(sprite)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    NINJA.setImage(assets.image`ninjaU`)
+    NINJA.setImage(ninjaU)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (NINJA.tileKindAt(TileDirection.Bottom, assets.tile`myTile8`) || NINJA.tileKindAt(TileDirection.Right, assets.tile`myTile8`) || (NINJA.tileKindAt(TileDirection.Top, assets.tile`myTile8`) || NINJA.tileKindAt(TileDirection.Left, assets.tile`myTile8`))) {
+        if (NINJA.image.equals(ninjaU)) {
+            game.showLongText("Fort Yaho", DialogLayout.Top)
+        }
+    }
+    if (NINJA.tileKindAt(TileDirection.Bottom, assets.tile`myTile5`) || NINJA.tileKindAt(TileDirection.Right, assets.tile`myTile5`) || (NINJA.tileKindAt(TileDirection.Top, assets.tile`myTile5`) || NINJA.tileKindAt(TileDirection.Left, assets.tile`myTile5`))) {
+        if (NINJA.image.equals(ninjaL)) {
+            game.showLongText("The Path Is Blocked", DialogLayout.Top)
+        }
+    }
+    if (NINJA.tileKindAt(TileDirection.Bottom, assets.tile`myTile10`) || NINJA.tileKindAt(TileDirection.Right, assets.tile`myTile10`) || (NINJA.tileKindAt(TileDirection.Top, assets.tile`myTile10`) || NINJA.tileKindAt(TileDirection.Left, assets.tile`myTile10`))) {
+        if (NINJA.image.equals(ninjaU)) {
+            if (NINJA.tilemapLocation() == tiles.getTileLocation(15, 18)) {
+                game.showLongText("The Door Is Locked", DialogLayout.Top)
+            }
+        }
+    }
+    if (tiles.tileAtLocationEquals(NINJA.tilemapLocation(), assets.tile`myTile9`)) {
+        game.showLongText("Digging In The Field ", DialogLayout.Top)
+    }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (NINJA.image.equals(assets.image`ninjaR`)) {
+    if (NINJA.image.equals(ninjaR)) {
         shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 100, 0)
-    } else if (NINJA.image.equals(assets.image`ninjaU`)) {
+    } else if (NINJA.image.equals(ninjaU)) {
         shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 0, -100)
-    } else if (NINJA.image.equals(assets.image`ninjaL`)) {
+    } else if (NINJA.image.equals(ninjaL)) {
         shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, -100, 0)
-    } else if (NINJA.image.equals(assets.image`ninjaD`)) {
+    } else if (NINJA.image.equals(ninjaD)) {
         shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 0, 100)
     }
     shuriken.setKind(SpriteKind.shuriken)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    NINJA.setImage(assets.image`ninjaL`)
+    NINJA.setImage(ninjaL)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+    	
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
@@ -42,13 +69,24 @@ scene.onOverlapTile(SpriteKind.shuriken, assets.tile`myTile0`, function (sprite,
     sprites.destroy(sprite)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    NINJA.setImage(assets.image`ninjaR`)
+    NINJA.setImage(ninjaR)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    NINJA.setImage(assets.image`ninjaD`)
+    NINJA.setImage(ninjaD)
 })
+/**
+ * fix door
+ */
 let shuriken: Sprite = null
 let NINJA: Sprite = null
+let ninjaU: Image = null
+let ninjaD: Image = null
+let ninjaL: Image = null
+let ninjaR: Image = null
+ninjaR = assets.image`ninjaR`
+ninjaL = assets.image`ninjaL`
+ninjaD = assets.image`ninjaD`
+ninjaU = assets.image`ninjaU`
 info.setLife(3)
 NINJA = sprites.create(assets.image`ninjaD`, SpriteKind.Player)
 controller.moveSprite(NINJA)
