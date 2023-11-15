025dda5869a6fa588cf0a6decbaee58f79f645b7
DLN-002 <146394752+DLN-002@users.noreply.github.com>
2023-11-15 15:34:32 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -60,25 +60,6 @@ f f e e f b f e e f f . .
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
-            case "image1":
-            case "ninjaD":return img`
-. . . . f f f f . . . . 
-. . f f f f f f f f . . 
-. f f f f f f c f f f . 
-f f f f f f c c f f f c 
-f f f c f f f f f f f c 
-c c c f f f e e f f c c 
-f f f f f e e f f c c f 
-f f f b f e e f b f f f 
-. f 4 1 f 4 4 f 1 4 f . 
-. f e 4 4 4 4 4 4 e f . 
-. f f f e e e e f f f . 
-f e f f f f f f f f e f 
-e 4 1 1 1 1 1 1 1 1 4 e 
-e e f f f 1 f f f f e e 
-. . . f f f f f f . . . 
-. . . f f . . f f . . . 
-`;
             case "image4":
             case "ninjaU":return img`
 . . . . f f f f . . . . 
@@ -95,6 +76,25 @@ f f f f f f f f f f f f
 f e f f f f f f f f e f 
 e 4 1 1 1 1 1 1 1 1 4 e 
 e e f f f f f f f f e e 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
+            case "image1":
+            case "ninjaD":return img`
+. . . . f f f f . . . . 
+. . f f f f f f f f . . 
+. f f f f f f c f f f . 
+f f f f f f c c f f f c 
+f f f c f f f f f f f c 
+c c c f f f e e f f c c 
+f f f f f e e f f c c f 
+f f f b f e e f b f f f 
+. f 4 1 f 4 4 f 1 4 f . 
+. f e 4 4 4 4 4 4 e f . 
+. f f f e e e e f f f . 
+f e f f f f f f f f e f 
+e 4 1 1 1 1 1 1 1 1 4 e 
+e e f f f 1 f f f f e e 
 . . . f f f f f f . . . 
 . . . f f . . f f . . . 
 `;
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const shuriken = SpriteKind.create()
+    export const farmer = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
@@ -16,6 +17,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     NINJA.setImage(ninjaU)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(NINJA, 0, 0)
     if (NINJA.tileKindAt(TileDirection.Bottom, assets.tile`myTile8`) || NINJA.tileKindAt(TileDirection.Right, assets.tile`myTile8`) || (NINJA.tileKindAt(TileDirection.Top, assets.tile`myTile8`) || NINJA.tileKindAt(TileDirection.Left, assets.tile`myTile8`))) {
         if (NINJA.image.equals(ninjaU)) {
             game.showLongText("Fort Yaho", DialogLayout.Top)
@@ -26,34 +28,70 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             game.showLongText("The Path Is Blocked", DialogLayout.Top)
         }
     }
-    if (tiles.tileAtLocationEquals(NINJA.tilemapLocation(), assets.tile`myTile9`)) {
-        NINJA.sayText("Do you want to water the soil?", 2000, true)
+    if (tiles.tileAtLocationEquals(NINJA.tilemapLocation(), assets.tile`myTile12`)) {
+        interacting = true
+        NINJA.sayText("Do you want to water the Sprout?", 2000, true)
         pause(2000)
-        NINJA.sayText("UP=Yes DOWN=No", 2000, true)
+        NINJA.sayText("A=Yes B=No", 2000, true)
         pause(2000)
         pauseUntil(() => controller.A.isPressed() || controller.B.isPressed())
-        if (controller.up.isPressed()) {
+        if (controller.A.isPressed()) {
             tiles.setTileAt(NINJA.tilemapLocation(), assets.tile`myTile11`)
-            NINJA.sayText("a plant sprouted!", 2000, true)
+            plants += 1
+            NINJA.sayText("the plant got larger!", 2000, true)
             pause(2000)
         }
-        if (controller.down.isPressed()) {
+        if (controller.B.isPressed()) {
             NINJA.sayText("well ok then...", 2000, true)
             pause(2000)
         }
+        interacting = false
     }
+    if (tiles.tileAtLocationEquals(NINJA.tilemapLocation(), assets.tile`myTile9`)) {
+        interacting = true
+        NINJA.sayText("Do you want to water the soil?", 2000, true)
+        pause(2000)
+        NINJA.sayText("A=Yes B=No", 2000, true)
+        pause(2000)
+        pauseUntil(() => controller.A.isPressed() || controller.B.isPressed())
+        if (controller.A.isPressed()) {
+            tiles.setTileAt(NINJA.tilemapLocation(), assets.tile`myTile12`)
+            NINJA.sayText("a plant sprouted!", 2000, true)
+            pause(2000)
+        }
+        if (controller.B.isPressed()) {
+            NINJA.sayText("well ok then...", 2000, true)
+            pause(2000)
+        }
+        interacting = false
+    }
+    if (NINJA.overlapsWith(farmer)) {
+        if (plants == 31) {
+            farmer.sayText("you be a good farmer, take this!", 2000, true)
+            pause(2000)
+            music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+            info.changeScoreBy(10)
+            sprites.destroy(farmer)
+        } else {
+            farmer.sayText("learn to be a good farmer and me give some you something good!", 2000, true)
+            pause(2000)
+        }
+    }
+    controller.moveSprite(NINJA)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (NINJA.image.equals(ninjaR)) {
-        shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 100, 0)
-    } else if (NINJA.image.equals(ninjaU)) {
-        shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 0, -100)
-    } else if (NINJA.image.equals(ninjaL)) {
-        shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, -100, 0)
-    } else if (NINJA.image.equals(ninjaD)) {
-        shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 0, 100)
+    if (interacting == false) {
+        if (NINJA.image.equals(ninjaR)) {
+            shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 100, 0)
+        } else if (NINJA.image.equals(ninjaU)) {
+            shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 0, -100)
+        } else if (NINJA.image.equals(ninjaL)) {
+            shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, -100, 0)
+        } else if (NINJA.image.equals(ninjaD)) {
+            shuriken = sprites.createProjectileFromSprite(assets.image`shuriken`, NINJA, 0, 100)
+        }
+        shuriken.setKind(SpriteKind.shuriken)
     }
-    shuriken.setKind(SpriteKind.shuriken)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     NINJA.setImage(ninjaL)
@@ -76,11 +114,15 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     NINJA.setImage(ninjaD)
 })
 let shuriken: Sprite = null
+let plants = 0
+let farmer: Sprite = null
 let NINJA: Sprite = null
 let ninjaU: Image = null
 let ninjaD: Image = null
 let ninjaL: Image = null
 let ninjaR: Image = null
+let interacting = false
+interacting = false
 ninjaR = assets.image`ninjaR`
 ninjaL = assets.image`ninjaL`
 ninjaD = assets.image`ninjaD`
@@ -91,3 +133,22 @@ controller.moveSprite(NINJA)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(NINJA)
 tiles.placeOnTile(NINJA, tiles.getTileLocation(6, 8))
+farmer = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f 7 7 7 7 7 7 f 4 e 
+    4 d f 7 7 7 7 7 7 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.farmer)
+tiles.placeOnTile(farmer, tiles.getTileLocation(29, 8))
